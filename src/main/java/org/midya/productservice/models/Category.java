4c4b0df908699c4a90738a76880de3c3c3309b1b
Om-Midya <dukemidya7521@gmail.com>
2024-04-11 20:19:49 +0530

    integrated and connected MySQL database for making self-ProductService
    Signed-off-by: Om-Midya <dukemidya7521@gmail.com>

--- src/main/java/org/midya/productservice/models/Category.java
+++ src/main/java/org/midya/productservice/models/Category.java
@@ -1,12 +1,16 @@
 package org.midya.productservice.models;
 
 import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
+    @Id
     private Long id;
     private String name;
 }
